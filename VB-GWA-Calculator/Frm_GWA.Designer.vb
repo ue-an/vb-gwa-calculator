0b0e4708f7dd0395d86dbbd1feaa944681ab5ae4
ue-an <fujisakiian@gmail.com>
2023-08-08 07:56:58 +0800

    retrieved from old projects

---
+++ VB-GWA-Calculator/Frm_GWA.Designer.vb
@@ -0,0 +1,377 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_GWA
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblGrades = New System.Windows.Forms.Label()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtGrd01 = New System.Windows.Forms.TextBox()
+        Me.txtGrd02 = New System.Windows.Forms.TextBox()
+        Me.txtGrd03 = New System.Windows.Forms.TextBox()
+        Me.txtGrd04 = New System.Windows.Forms.TextBox()
+        Me.txtGrd05 = New System.Windows.Forms.TextBox()
+        Me.txtGrd06 = New System.Windows.Forms.TextBox()
+        Me.txtGrd07 = New System.Windows.Forms.TextBox()
+        Me.txtGrd08 = New System.Windows.Forms.TextBox()
+        Me.txtUnit01 = New System.Windows.Forms.TextBox()
+        Me.txtUnit02 = New System.Windows.Forms.TextBox()
+        Me.txtUnit03 = New System.Windows.Forms.TextBox()
+        Me.txtUnit04 = New System.Windows.Forms.TextBox()
+        Me.txtUnit05 = New System.Windows.Forms.TextBox()
+        Me.txtUnit06 = New System.Windows.Forms.TextBox()
+        Me.txtUnit07 = New System.Windows.Forms.TextBox()
+        Me.txtUnit08 = New System.Windows.Forms.TextBox()
+        Me.lblGrdSum = New System.Windows.Forms.Label()
+        Me.lblUnitSum = New System.Windows.Forms.Label()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblGWA = New System.Windows.Forms.Label()
+        Me.txtGrd09 = New System.Windows.Forms.TextBox()
+        Me.txtUnit09 = New System.Windows.Forms.TextBox()
+        Me.cmbSubjects = New System.Windows.Forms.ComboBox()
+        Me.lblSubjects = New System.Windows.Forms.Label()
+        Me.lblGwSum = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblGrades
+        '
+        Me.lblGrades.AutoSize = True
+        Me.lblGrades.Location = New System.Drawing.Point(119, 18)
+        Me.lblGrades.Name = "lblGrades"
+        Me.lblGrades.Size = New System.Drawing.Size(63, 19)
+        Me.lblGrades.TabIndex = 0
+        Me.lblGrades.Text = "Grades"
+        '
+        'lblUnits
+        '
+        Me.lblUnits.AutoSize = True
+        Me.lblUnits.Location = New System.Drawing.Point(279, 18)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(54, 19)
+        Me.lblUnits.TabIndex = 1
+        Me.lblUnits.Text = "Units"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(150, 368)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 19)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Average"
+        '
+        'txtGrd01
+        '
+        Me.txtGrd01.Enabled = False
+        Me.txtGrd01.Location = New System.Drawing.Point(106, 51)
+        Me.txtGrd01.Name = "txtGrd01"
+        Me.txtGrd01.Size = New System.Drawing.Size(100, 26)
+        Me.txtGrd01.TabIndex = 3
+        '
+        'txtGrd02
+        '
+        Me.txtGrd02.Enabled = False
+        Me.txtGrd02.Location = New System.Drawing.Point(106, 83)
+        Me.txtGrd02.Name = "txtGrd02"
+        Me.txtGrd02.Size = New System.Drawing.Size(100, 26)
+        Me.txtGrd02.TabIndex = 4
+        '
+        'txtGrd03
+        '
+        Me.txtGrd03.Enabled = False
+        Me.txtGrd03.Location = New System.Drawing.Point(106, 115)
+        Me.txtGrd03.Name = "txtGrd03"
+        Me.txtGrd03.Size = New System.Drawing.Size(100, 26)
+        Me.txtGrd03.TabIndex = 4
+        '
+        'txtGrd04
+        '
+        Me.txtGrd04.Enabled = False
+        Me.txtGrd04.Location = New System.Drawing.Point(106, 147)
+        Me.txtGrd04.Name = "txtGrd04"
+        Me.txtGrd04.Size = New System.Drawing.Size(100, 26)
+        Me.txtGrd04.TabIndex = 4
+        '
+        'txtGrd05
+        '
+        Me.txtGrd05.Enabled = False
+        Me.txtGrd05.Location = New System.Drawing.Point(106, 179)
+        Me.txtGrd05.Name = "txtGrd05"
+        Me.txtGrd05.Size = New System.Drawing.Size(100, 26)
+        Me.txtGrd05.TabIndex = 4
+        '
+        'txtGrd06
+        '
+        Me.txtGrd06.Enabled = False
+        Me.txtGrd06.Location = New System.Drawing.Point(106, 211)
+        Me.txtGrd06.Name = "txtGrd06"
+        Me.txtGrd06.Size = New System.Drawing.Size(100, 26)
+        Me.txtGrd06.TabIndex = 4
+        '
+        'txtGrd07
+        '
+        Me.txtGrd07.Enabled = False
+        Me.txtGrd07.Location = New System.Drawing.Point(106, 243)
+        Me.txtGrd07.Name = "txtGrd07"
+        Me.txtGrd07.Size = New System.Drawing.Size(100, 26)
+        Me.txtGrd07.TabIndex = 4
+        '
+        'txtGrd08
+        '
+        Me.txtGrd08.Enabled = False
+        Me.txtGrd08.Location = New System.Drawing.Point(106, 275)
+        Me.txtGrd08.Name = "txtGrd08"
+        Me.txtGrd08.Size = New System.Drawing.Size(100, 26)
+        Me.txtGrd08.TabIndex = 4
+        '
+        'txtUnit01
+        '
+        Me.txtUnit01.Enabled = False
+        Me.txtUnit01.Location = New System.Drawing.Point(258, 51)
+        Me.txtUnit01.Name = "txtUnit01"
+        Me.txtUnit01.Size = New System.Drawing.Size(100, 26)
+        Me.txtUnit01.TabIndex = 3
+        '
+        'txtUnit02
+        '
+        Me.txtUnit02.Enabled = False
+        Me.txtUnit02.Location = New System.Drawing.Point(258, 83)
+        Me.txtUnit02.Name = "txtUnit02"
+        Me.txtUnit02.Size = New System.Drawing.Size(100, 26)
+        Me.txtUnit02.TabIndex = 4
+        '
+        'txtUnit03
+        '
+        Me.txtUnit03.Enabled = False
+        Me.txtUnit03.Location = New System.Drawing.Point(258, 115)
+        Me.txtUnit03.Name = "txtUnit03"
+        Me.txtUnit03.Size = New System.Drawing.Size(100, 26)
+        Me.txtUnit03.TabIndex = 4
+        '
+        'txtUnit04
+        '
+        Me.txtUnit04.Enabled = False
+        Me.txtUnit04.Location = New System.Drawing.Point(258, 147)
+        Me.txtUnit04.Name = "txtUnit04"
+        Me.txtUnit04.Size = New System.Drawing.Size(100, 26)
+        Me.txtUnit04.TabIndex = 4
+        '
+        'txtUnit05
+        '
+        Me.txtUnit05.Enabled = False
+        Me.txtUnit05.Location = New System.Drawing.Point(258, 179)
+        Me.txtUnit05.Name = "txtUnit05"
+        Me.txtUnit05.Size = New System.Drawing.Size(100, 26)
+        Me.txtUnit05.TabIndex = 4
+        '
+        'txtUnit06
+        '
+        Me.txtUnit06.Enabled = False
+        Me.txtUnit06.Location = New System.Drawing.Point(258, 211)
+        Me.txtUnit06.Name = "txtUnit06"
+        Me.txtUnit06.Size = New System.Drawing.Size(100, 26)
+        Me.txtUnit06.TabIndex = 4
+        '
+        'txtUnit07
+        '
+        Me.txtUnit07.Enabled = False
+        Me.txtUnit07.Location = New System.Drawing.Point(258, 243)
+        Me.txtUnit07.Name = "txtUnit07"
+        Me.txtUnit07.Size = New System.Drawing.Size(100, 26)
+        Me.txtUnit07.TabIndex = 4
+        '
+        'txtUnit08
+        '
+        Me.txtUnit08.Enabled = False
+        Me.txtUnit08.Location = New System.Drawing.Point(258, 275)
+        Me.txtUnit08.Name = "txtUnit08"
+        Me.txtUnit08.Size = New System.Drawing.Size(100, 26)
+        Me.txtUnit08.TabIndex = 4
+        '
+        'lblGrdSum
+        '
+        Me.lblGrdSum.BackColor = System.Drawing.Color.White
+        Me.lblGrdSum.Location = New System.Drawing.Point(364, 73)
+        Me.lblGrdSum.Name = "lblGrdSum"
+        Me.lblGrdSum.Size = New System.Drawing.Size(63, 23)
+        Me.lblGrdSum.TabIndex = 5
+        Me.lblGrdSum.Text = "0"
+        Me.lblGrdSum.Visible = False
+        '
+        'lblUnitSum
+        '
+        Me.lblUnitSum.BackColor = System.Drawing.Color.White
+        Me.lblUnitSum.Location = New System.Drawing.Point(364, 119)
+        Me.lblUnitSum.Name = "lblUnitSum"
+        Me.lblUnitSum.Size = New System.Drawing.Size(63, 22)
+        Me.lblUnitSum.TabIndex = 5
+        Me.lblUnitSum.Text = "0"
+        Me.lblUnitSum.Visible = False
+        '
+        'lblAverage
+        '
+        Me.lblAverage.BackColor = System.Drawing.Color.White
+        Me.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAverage.Location = New System.Drawing.Point(233, 367)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(100, 24)
+        Me.lblAverage.TabIndex = 6
+        Me.lblAverage.Text = "0"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(150, 408)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 19)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "GWA/QPA"
+        '
+        'lblGWA
+        '
+        Me.lblGWA.BackColor = System.Drawing.Color.White
+        Me.lblGWA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGWA.Location = New System.Drawing.Point(233, 407)
+        Me.lblGWA.Name = "lblGWA"
+        Me.lblGWA.Size = New System.Drawing.Size(100, 23)
+        Me.lblGWA.TabIndex = 6
+        Me.lblGWA.Text = "0"
+        '
+        'txtGrd09
+        '
+        Me.txtGrd09.Enabled = False
+        Me.txtGrd09.Location = New System.Drawing.Point(106, 307)
+        Me.txtGrd09.Name = "txtGrd09"
+        Me.txtGrd09.Size = New System.Drawing.Size(100, 26)
+        Me.txtGrd09.TabIndex = 4
+        '
+        'txtUnit09
+        '
+        Me.txtUnit09.Enabled = False
+        Me.txtUnit09.Location = New System.Drawing.Point(258, 307)
+        Me.txtUnit09.Name = "txtUnit09"
+        Me.txtUnit09.Size = New System.Drawing.Size(100, 26)
+        Me.txtUnit09.TabIndex = 4
+        '
+        'cmbSubjects
+        '
+        Me.cmbSubjects.FormattingEnabled = True
+        Me.cmbSubjects.Location = New System.Drawing.Point(16, 51)
+        Me.cmbSubjects.Name = "cmbSubjects"
+        Me.cmbSubjects.Size = New System.Drawing.Size(49, 27)
+        Me.cmbSubjects.TabIndex = 7
+        '
+        'lblSubjects
+        '
+        Me.lblSubjects.AutoSize = True
+        Me.lblSubjects.Location = New System.Drawing.Point(12, 18)
+        Me.lblSubjects.Name = "lblSubjects"
+        Me.lblSubjects.Size = New System.Drawing.Size(81, 19)
+        Me.lblSubjects.TabIndex = 8
+        Me.lblSubjects.Text = "Subjects"
+        '
+        'lblGwSum
+        '
+        Me.lblGwSum.BackColor = System.Drawing.Color.White
+        Me.lblGwSum.Location = New System.Drawing.Point(364, 160)
+        Me.lblGwSum.Name = "lblGwSum"
+        Me.lblGwSum.Size = New System.Drawing.Size(63, 23)
+        Me.lblGwSum.TabIndex = 9
+        Me.lblGwSum.Text = "0"
+        Me.lblGwSum.Visible = False
+        '
+        'Frm_GWA
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(449, 476)
+        Me.Controls.Add(Me.lblGwSum)
+        Me.Controls.Add(Me.lblSubjects)
+        Me.Controls.Add(Me.cmbSubjects)
+        Me.Controls.Add(Me.lblGWA)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.lblUnitSum)
+        Me.Controls.Add(Me.lblGrdSum)
+        Me.Controls.Add(Me.txtUnit09)
+        Me.Controls.Add(Me.txtUnit08)
+        Me.Controls.Add(Me.txtGrd09)
+        Me.Controls.Add(Me.txtGrd08)
+        Me.Controls.Add(Me.txtUnit07)
+        Me.Controls.Add(Me.txtGrd07)
+        Me.Controls.Add(Me.txtUnit06)
+        Me.Controls.Add(Me.txtGrd06)
+        Me.Controls.Add(Me.txtUnit05)
+        Me.Controls.Add(Me.txtGrd05)
+        Me.Controls.Add(Me.txtUnit04)
+        Me.Controls.Add(Me.txtGrd04)
+        Me.Controls.Add(Me.txtUnit03)
+        Me.Controls.Add(Me.txtGrd03)
+        Me.Controls.Add(Me.txtUnit02)
+        Me.Controls.Add(Me.txtGrd02)
+        Me.Controls.Add(Me.txtUnit01)
+        Me.Controls.Add(Me.txtGrd01)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblUnits)
+        Me.Controls.Add(Me.lblGrades)
+        Me.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Frm_GWA"
+        Me.Text = "DOST GWA Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblGrades As System.Windows.Forms.Label
+    Friend WithEvents lblUnits As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtGrd01 As System.Windows.Forms.TextBox
+    Friend WithEvents txtGrd02 As System.Windows.Forms.TextBox
+    Friend WithEvents txtGrd03 As System.Windows.Forms.TextBox
+    Friend WithEvents txtGrd04 As System.Windows.Forms.TextBox
+    Friend WithEvents txtGrd05 As System.Windows.Forms.TextBox
+    Friend WithEvents txtGrd06 As System.Windows.Forms.TextBox
+    Friend WithEvents txtGrd07 As System.Windows.Forms.TextBox
+    Friend WithEvents txtGrd08 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUnit01 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUnit02 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUnit03 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUnit04 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUnit05 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUnit06 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUnit07 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUnit08 As System.Windows.Forms.TextBox
+    Friend WithEvents lblGrdSum As System.Windows.Forms.Label
+    Friend WithEvents lblUnitSum As System.Windows.Forms.Label
+    Friend WithEvents lblAverage As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblGWA As System.Windows.Forms.Label
+    Friend WithEvents txtGrd09 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUnit09 As System.Windows.Forms.TextBox
+    Friend WithEvents cmbSubjects As System.Windows.Forms.ComboBox
+    Friend WithEvents lblSubjects As System.Windows.Forms.Label
+    Friend WithEvents lblGwSum As System.Windows.Forms.Label
+
+End Class
---
+++ VB-GWA-Calculator/Frm_GWA.vb
@@ -0,0 +1,362 @@
+﻿Public Class Frm_GWA
+
+    Private Sub Frm_GWA_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For subs = 1 To 9
+            cmbSubjects.Items.Add(subs)
+        Next
+    End Sub
+
+    Private Sub ComputeAVE()
+        Dim grdSum = Val(txtGrd01.Text) + Val(txtGrd02.Text) + Val(txtGrd03.Text) + Val(txtGrd04.Text) +
+                   Val(txtGrd05.Text) + Val(txtGrd06.Text) + Val(txtGrd07.Text) + Val(txtGrd08.Text) + Val(txtGrd09.Text)
+        Dim ave = grdSum / Val(cmbSubjects.Text)
+
+        lblGrdSum.Text = grdSum
+        lblAverage.Text = Format(ave, "#,###,##0.000")
+    End Sub
+
+    Private Sub ComputeGWA()
+
+        Dim gw1 = Val(txtGrd01.Text) * Val(txtUnit01.Text)
+        Dim gw2 = Val(txtGrd02.Text) * Val(txtUnit02.Text)
+        Dim gw3 = Val(txtGrd03.Text) * Val(txtUnit03.Text)
+        Dim gw4 = Val(txtGrd04.Text) * Val(txtUnit04.Text)
+        Dim gw5 = Val(txtGrd05.Text) * Val(txtUnit05.Text)
+        Dim gw6 = Val(txtGrd06.Text) * Val(txtUnit06.Text)
+        Dim gw7 = Val(txtGrd07.Text) * Val(txtUnit07.Text)
+        Dim gw8 = Val(txtGrd08.Text) * Val(txtUnit08.Text)
+        Dim gw9 = Val(txtGrd09.Text) * Val(txtUnit09.Text)
+
+        Dim gwSum = gw1 + gw2 + gw3 + gw4 + gw5 + gw6 + gw7 + gw8 + gw9
+        Dim unitSum = Val(txtUnit01.Text) + Val(txtUnit02.Text) + Val(txtUnit03.Text) + Val(txtUnit04.Text) +
+                      Val(txtUnit05.Text) + Val(txtUnit06.Text) + Val(txtUnit07.Text) + Val(txtUnit08.Text) +
+                      Val(txtUnit09.Text)
+
+        Dim gwa = gwSum / unitSum
+
+        lblUnitSum.Text = unitSum
+        lblGwSum.Text = gwSum
+        lblGWA.Text = Format(gwa, "#,###,##0.000")
+    End Sub
+
+    Private Sub EnableGradeUnit01()
+        txtGrd01.Enabled = True
+        txtUnit01.Enabled = True
+        txtGrd02.Enabled = False
+        txtUnit02.Enabled = False
+        txtGrd03.Enabled = False
+        txtUnit03.Enabled = False
+        txtGrd04.Enabled = False
+        txtUnit04.Enabled = False
+        txtGrd05.Enabled = False
+        txtUnit05.Enabled = False
+        txtGrd06.Enabled = False
+        txtUnit06.Enabled = False
+        txtGrd07.Enabled = False
+        txtUnit07.Enabled = False
+        txtGrd08.Enabled = False
+        txtUnit08.Enabled = False
+        txtGrd09.Enabled = False
+        txtUnit09.Enabled = False
+    End Sub
+
+    Private Sub EnableGradeUnit02()
+        txtGrd01.Enabled = True
+        txtUnit01.Enabled = True
+        txtGrd02.Enabled = True
+        txtUnit02.Enabled = True
+        txtGrd03.Enabled = False
+        txtUnit03.Enabled = False
+        txtGrd04.Enabled = False
+        txtUnit04.Enabled = False
+        txtGrd05.Enabled = False
+        txtUnit05.Enabled = False
+        txtGrd06.Enabled = False
+        txtUnit06.Enabled = False
+        txtGrd07.Enabled = False
+        txtUnit07.Enabled = False
+        txtGrd08.Enabled = False
+        txtUnit08.Enabled = False
+        txtGrd09.Enabled = False
+        txtUnit09.Enabled = False
+    End Sub
+
+    Private Sub EnableGradeUnit03()
+        txtGrd01.Enabled = True
+        txtUnit01.Enabled = True
+        txtGrd02.Enabled = True
+        txtUnit02.Enabled = True
+        txtGrd03.Enabled = True
+        txtUnit03.Enabled = True
+        txtGrd04.Enabled = False
+        txtUnit04.Enabled = False
+        txtGrd05.Enabled = False
+        txtUnit05.Enabled = False
+        txtGrd06.Enabled = False
+        txtUnit06.Enabled = False
+        txtGrd07.Enabled = False
+        txtUnit07.Enabled = False
+        txtGrd08.Enabled = False
+        txtUnit08.Enabled = False
+        txtGrd09.Enabled = False
+        txtUnit09.Enabled = False
+    End Sub
+
+    Private Sub EnableGradeUnit04()
+        txtGrd01.Enabled = True
+        txtUnit01.Enabled = True
+        txtGrd02.Enabled = True
+        txtUnit02.Enabled = True
+        txtGrd03.Enabled = True
+        txtUnit03.Enabled = True
+        txtGrd04.Enabled = True
+        txtUnit04.Enabled = True
+        txtGrd05.Enabled = False
+        txtUnit05.Enabled = False
+        txtGrd06.Enabled = False
+        txtUnit06.Enabled = False
+        txtGrd07.Enabled = False
+        txtUnit07.Enabled = False
+        txtGrd08.Enabled = False
+        txtUnit08.Enabled = False
+        txtGrd09.Enabled = False
+        txtUnit09.Enabled = False
+    End Sub
+
+    Private Sub EnableGradeUnit05()
+        txtGrd01.Enabled = True
+        txtUnit01.Enabled = True
+        txtGrd02.Enabled = True
+        txtUnit02.Enabled = True
+        txtGrd03.Enabled = True
+        txtUnit03.Enabled = True
+        txtGrd04.Enabled = True
+        txtUnit04.Enabled = True
+        txtGrd05.Enabled = True
+        txtUnit05.Enabled = True
+        txtGrd06.Enabled = False
+        txtUnit06.Enabled = False
+        txtGrd07.Enabled = False
+        txtUnit07.Enabled = False
+        txtGrd08.Enabled = False
+        txtUnit08.Enabled = False
+        txtGrd09.Enabled = False
+        txtUnit09.Enabled = False
+    End Sub
+
+    Private Sub EnableGradeUnit06()
+        txtGrd01.Enabled = True
+        txtUnit01.Enabled = True
+        txtGrd02.Enabled = True
+        txtUnit02.Enabled = True
+        txtGrd03.Enabled = True
+        txtUnit03.Enabled = True
+        txtGrd04.Enabled = True
+        txtUnit04.Enabled = True
+        txtGrd05.Enabled = True
+        txtUnit05.Enabled = True
+        txtGrd06.Enabled = True
+        txtUnit06.Enabled = True
+        txtGrd07.Enabled = False
+        txtUnit07.Enabled = False
+        txtGrd08.Enabled = False
+        txtUnit08.Enabled = False
+        txtGrd09.Enabled = False
+        txtUnit09.Enabled = False
+    End Sub
+
+    Private Sub EnableGradeUnit07()
+        txtGrd01.Enabled = True
+        txtUnit01.Enabled = True
+        txtGrd02.Enabled = True
+        txtUnit02.Enabled = True
+        txtGrd03.Enabled = True
+        txtUnit03.Enabled = True
+        txtGrd04.Enabled = True
+        txtUnit04.Enabled = True
+        txtGrd05.Enabled = True
+        txtUnit05.Enabled = True
+        txtGrd06.Enabled = True
+        txtUnit06.Enabled = True
+        txtGrd07.Enabled = True
+        txtUnit07.Enabled = True
+        txtGrd08.Enabled = False
+        txtUnit08.Enabled = False
+        txtGrd09.Enabled = False
+        txtUnit09.Enabled = False
+    End Sub
+
+    Private Sub EnableGradeUnit08()
+        txtGrd01.Enabled = True
+        txtUnit01.Enabled = True
+        txtGrd02.Enabled = True
+        txtUnit02.Enabled = True
+        txtGrd03.Enabled = True
+        txtUnit03.Enabled = True
+        txtGrd04.Enabled = True
+        txtUnit04.Enabled = True
+        txtGrd05.Enabled = True
+        txtUnit05.Enabled = True
+        txtGrd06.Enabled = True
+        txtUnit06.Enabled = True
+        txtGrd07.Enabled = True
+        txtUnit07.Enabled = True
+        txtGrd08.Enabled = True
+        txtUnit08.Enabled = True
+        txtGrd09.Enabled = False
+        txtUnit09.Enabled = False
+    End Sub
+
+    Private Sub EnableGradeUnit09()
+        txtGrd01.Enabled = True
+        txtUnit01.Enabled = True
+        txtGrd02.Enabled = True
+        txtUnit02.Enabled = True
+        txtGrd03.Enabled = True
+        txtUnit03.Enabled = True
+        txtGrd04.Enabled = True
+        txtUnit04.Enabled = True
+        txtGrd05.Enabled = True
+        txtUnit05.Enabled = True
+        txtGrd06.Enabled = True
+        txtUnit06.Enabled = True
+        txtGrd07.Enabled = True
+        txtUnit07.Enabled = True
+        txtGrd08.Enabled = True
+        txtUnit08.Enabled = True
+        txtGrd09.Enabled = True
+        txtUnit09.Enabled = True
+    End Sub
+
+    Private Sub cmbSubjects_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbSubjects.SelectedIndexChanged
+        Select Case cmbSubjects.Text
+            Case 1
+                EnableGradeUnit01()
+            Case 2
+                EnableGradeUnit01()
+                EnableGradeUnit02()
+            Case 3
+                EnableGradeUnit01()
+                EnableGradeUnit02()
+                EnableGradeUnit03()
+            Case 4
+                EnableGradeUnit01()
+                EnableGradeUnit02()
+                EnableGradeUnit03()
+                EnableGradeUnit04()
+            Case 5
+                EnableGradeUnit01()
+                EnableGradeUnit02()
+                EnableGradeUnit03()
+                EnableGradeUnit04()
+                EnableGradeUnit05()
+            Case 6
+                EnableGradeUnit01()
+                EnableGradeUnit02()
+                EnableGradeUnit03()
+                EnableGradeUnit04()
+                EnableGradeUnit05()
+                EnableGradeUnit06()
+            Case 7
+                EnableGradeUnit01()
+                EnableGradeUnit02()
+                EnableGradeUnit03()
+                EnableGradeUnit04()
+                EnableGradeUnit05()
+                EnableGradeUnit06()
+                EnableGradeUnit07()
+            Case 8
+                EnableGradeUnit01()
+                EnableGradeUnit02()
+                EnableGradeUnit03()
+                EnableGradeUnit04()
+                EnableGradeUnit05()
+                EnableGradeUnit06()
+                EnableGradeUnit07()
+                EnableGradeUnit08()
+            Case 9
+                EnableGradeUnit01()
+                EnableGradeUnit02()
+                EnableGradeUnit03()
+                EnableGradeUnit04()
+                EnableGradeUnit05()
+                EnableGradeUnit06()
+                EnableGradeUnit07()
+                EnableGradeUnit08()
+                EnableGradeUnit09()
+        End Select
+    End Sub
+
+    Private Sub txtGrd01_TextChanged(sender As Object, e As EventArgs) Handles txtGrd01.TextChanged
+        ComputeAVE()
+    End Sub
+
+    Private Sub txtGrd02_TextChanged(sender As Object, e As EventArgs) Handles txtGrd02.TextChanged
+        ComputeAVE()
+    End Sub
+
+    Private Sub txtGrd03_TextChanged(sender As Object, e As EventArgs) Handles txtGrd03.TextChanged
+        ComputeAVE()
+    End Sub
+
+    Private Sub txtGrd04_TextChanged(sender As Object, e As EventArgs) Handles txtGrd04.TextChanged
+        ComputeAVE()
+    End Sub
+
+    Private Sub txtGrd05_TextChanged(sender As Object, e As EventArgs) Handles txtGrd05.TextChanged
+        ComputeAVE()
+    End Sub
+
+    Private Sub txtGrd06_TextChanged(sender As Object, e As EventArgs) Handles txtGrd06.TextChanged
+        ComputeAVE()
+    End Sub
+
+    Private Sub txtGrd07_TextChanged(sender As Object, e As EventArgs) Handles txtGrd07.TextChanged
+        ComputeAVE()
+    End Sub
+
+    Private Sub txtGrd08_TextChanged(sender As Object, e As EventArgs) Handles txtGrd08.TextChanged
+        ComputeAVE()
+    End Sub
+
+    Private Sub txtGrd09_TextChanged(sender As Object, e As EventArgs) Handles txtGrd09.TextChanged
+        ComputeAVE()
+    End Sub
+
+
+    Private Sub txtUnit01_TextChanged(sender As Object, e As EventArgs) Handles txtUnit01.TextChanged
+        ComputeGWA()
+    End Sub
+
+    Private Sub txtUnit02_TextChanged(sender As Object, e As EventArgs) Handles txtUnit02.TextChanged
+        ComputeGWA()
+    End Sub
+
+    Private Sub txtUnit03_TextChanged(sender As Object, e As EventArgs) Handles txtUnit03.TextChanged
+        ComputeGWA()
+    End Sub
+
+    Private Sub txtUnit04_TextChanged(sender As Object, e As EventArgs) Handles txtUnit04.TextChanged
+        ComputeGWA()
+    End Sub
+
+    Private Sub txtUnit05_TextChanged(sender As Object, e As EventArgs) Handles txtUnit05.TextChanged
+        ComputeGWA()
+    End Sub
+
+    Private Sub txtUnit06_TextChanged(sender As Object, e As EventArgs) Handles txtUnit06.TextChanged
+        ComputeGWA()
+    End Sub
+
+    Private Sub txtUnit07_TextChanged(sender As Object, e As EventArgs) Handles txtUnit07.TextChanged
+        ComputeGWA()
+    End Sub
+
+    Private Sub txtUnit08_TextChanged(sender As Object, e As EventArgs) Handles txtUnit08.TextChanged
+        ComputeGWA()
+    End Sub
+
+    Private Sub txtUnit09_TextChanged(sender As Object, e As EventArgs) Handles txtUnit09.TextChanged
+        ComputeGWA()
+    End Sub
+End Class
